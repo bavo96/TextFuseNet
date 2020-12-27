36c8fdaaa072bcdc13fb2f492d54bd53879735f5
Bang Vo <vqbang1996@gmail.com>
2020-12-27 17:33:42 +0700

    Update demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -21,16 +21,17 @@ def save_result_to_txt(txt_save_path,prediction):
     file = open(txt_save_path,'w')
     classes = prediction['instances'].pred_classes
     boxes = prediction['instances'].pred_boxes.tensor
-
+    scores = prediction['instances'].scores.tensor
+    
     for i in range(len(classes)):
-        if classes[i]==0:
-            xmin = str(int(boxes[i][0]))
-            ymin = str(int(boxes[i][1]))
-            xmax = str(int(boxes[i][2]))
-            ymax = str(int(boxes[i][3]))
-
-            file.writelines(xmin+','+ymin+','+xmax+','+ymax+',')
-            file.writelines('\r\n')
+        #if classes[i]==0:
+        xmin = str(int(boxes[i][0]))
+        ymin = str(int(boxes[i][1]))
+        xmax = str(int(boxes[i][2]))
+        ymax = str(int(boxes[i][3]))
+        
+        file.writelines(xmin+','+ymin+','+xmax+','+ymax+','+classes[i]+scores[i]+'\n')
+        
     file.close()
 
 def setup_cfg():
